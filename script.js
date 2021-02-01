27a467c207671bfd915a7db74b9e46ac99ef28f6
victorfrankenstein245 <56684147+victorfrankenstein245@users.noreply.github.com>
2021-02-01 21:09:26 +0100

    js file completed

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ sounds.forEach((sound) => {
 
   btn.innerText = sound;
 
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).onplay();
+  });
+
   document.getElementById("buttons").appendChild(btn);
 });
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
